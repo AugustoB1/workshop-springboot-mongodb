1fdb9fab1565b88a5044de3791be975e6a2548b7
AugustoB1 <guto.brito280802@gmail.com>
2023-07-19 21:08:45 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/augustobr/workshopmongo/domain/User.java
+++ src/main/java/com/augustobr/workshopmongo/domain/User.java
@@ -3,9 +3,14 @@ package com.augustobr.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
@@ -49,7 +54,7 @@ public class User implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
+ 
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
